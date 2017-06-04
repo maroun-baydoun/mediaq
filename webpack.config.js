d81170b56652c9907e266894983c98aaca11cfd3
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-06-04 23:20:13 +0900

    Separated building mediaq from test and sample

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 var path = require('path');
 module.exports = {
   entry: {
-    mediaq: './mediaq.ts',
     'test/test': './test/test.ts',
     'sample/app': './sample/app.ts'
   },
   output: {
     filename: '[name].js',
-    publicPath: '/dist/',
-    path: path.resolve(__dirname, 'dist')
+    publicPath: '/dist/web/',
+    path: path.resolve(__dirname, 'dist/web')
   },
   resolve: {
     extensions: ['.ts']
