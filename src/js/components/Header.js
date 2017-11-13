2f586b5d00a66ab869ac4d284c06c2f1718c7e8f
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-11-13 13:53:16 +0900

    Added badges in header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -9,6 +9,16 @@ const Header = (props) => {
             <h1>Mediaq</h1>
             <h2>Listen to media queries updates in JavaScript</h2>
         </div>
+        <div className="badges-container">
+            <div className="npm-badge">
+                <a href="https://badge.fury.io/js/mediaq">
+                    <img src="https://badge.fury.io/js/mediaq.svg" alt="npm version" height="18"/>
+                </a>
+            </div>
+            <div className="github-badge">
+                <a className="github-button" href="https://github.com/maroun-baydoun/mediaq" data-show-count="true" aria-label="Star maroun-baydoun/new-hope on GitHub"></a>
+            </div>
+        </div>
     </header>
 }
 
